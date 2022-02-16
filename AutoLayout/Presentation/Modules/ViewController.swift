b0b125ba9eca6a6034303219c6054be90f39725b
Саидов Тимур <tsaidov@uma.tech>
2022-02-16 21:32:33 +0300

    Реализована ProfileHeaderView

--- AutoLayout/Presentation/Modules/ViewController.swift
+++ AutoLayout/Presentation/Modules/ViewController.swift
@@ -8,6 +8,15 @@
 import UIKit
 
 final class ViewController: UIViewController {
+    
+    private lazy var profileHeaderView: ProfileHeaderView = {
+        let view = ProfileHeaderView(frame: .zero)
+        view.delegate = self
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private var heightConstraint: NSLayoutConstraint?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,5 +31,29 @@ final class ViewController: UIViewController {
     
     private func setupView() {
         self.view.backgroundColor = .white
+        
+        self.view.addSubview(self.profileHeaderView)
+        
+        let topConstraint = self.profileHeaderView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor)
+        let leadingConstraint = self.profileHeaderView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor)
+        let trailingConstraint = self.profileHeaderView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor)
+        self.heightConstraint = self.profileHeaderView.heightAnchor.constraint(equalToConstant: 170)
+        
+        NSLayoutConstraint.activate([
+            topConstraint, leadingConstraint, trailingConstraint, self.heightConstraint
+        ].compactMap({ $0 }))
+    }
+}
+
+extension ViewController: ProfileHeaderViewProtocol {
+    
+    func didTapStatusButton(textFieldIsVisible: Bool, completion: @escaping () -> Void) {
+        self.heightConstraint?.constant = textFieldIsVisible ? 214 : 170
+        
+        UIView.animate(withDuration: 0.3, delay: 0.0) {
+            self.view.layoutIfNeeded()
+        } completion: { _ in
+            completion()
+        }
     }
 }
---
+++ AutoLayout/Presentation/Views/ProfileHeaderView.swift
@@ -0,0 +1,136 @@
+//
+//  ProfileHeaderView.swift
+//  AutoLayout
+//
+//  Created by Саидов Тимур on 16.02.2022.
+//
+
+import UIKit
+
+protocol ProfileHeaderViewProtocol: AnyObject {
+    func didTapStatusButton(textFieldIsVisible: Bool, completion: @escaping () -> Void)
+}
+
+final class ProfileHeaderView: UIView {
+    
+    private lazy var imageview: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .systemRed
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private lazy var nameLabel: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .systemGreen
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var statusLabel: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .blue
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var textField: UITextField = {
+        let textField = UITextField()
+        textField.isHidden = true
+        textField.backgroundColor = .systemYellow
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    private lazy var statusButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .systemOrange
+        button.addTarget(self, action: #selector(self.didTapStatusButton), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private lazy var labelsStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.spacing = 10
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private lazy var infoStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.spacing = 20
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private var buttonTopConstraint: NSLayoutConstraint?
+    
+    weak var delegate: ProfileHeaderViewProtocol?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.drawSelf()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func drawSelf() {
+        self.addSubview(self.infoStackView)
+        self.addSubview(self.statusButton)
+        self.addSubview(self.textField)
+        self.infoStackView.addArrangedSubview(self.imageview)
+        self.infoStackView.addArrangedSubview(self.labelsStackView)
+        self.labelsStackView.addArrangedSubview(self.nameLabel)
+        self.labelsStackView.addArrangedSubview(self.statusLabel)
+        
+        let topConstraint = self.infoStackView.topAnchor.constraint(equalTo: self.topAnchor)
+        let leadingConstraint = self.infoStackView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20)
+        let trailingConstraint = self.infoStackView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20)
+        
+        let imageViewAspectRatio = self.imageview.heightAnchor.constraint(equalTo: self.imageview.widthAnchor, multiplier: 1.0)
+        
+        self.buttonTopConstraint = self.statusButton.topAnchor.constraint(equalTo: self.infoStackView.bottomAnchor, constant: 20)
+        self.buttonTopConstraint?.priority = UILayoutPriority(rawValue: 999)
+        let leadingButtonConstraint = self.statusButton.leadingAnchor.constraint(equalTo: self.infoStackView.leadingAnchor)
+        let trailingButtonConstraint = self.statusButton.trailingAnchor.constraint(equalTo: self.infoStackView.trailingAnchor)
+        let bottomButtonConstraint = self.statusButton.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        let heightButtonConstraint = self.statusButton.heightAnchor.constraint(equalToConstant: 50) // Не указав высоту statusButton'а, получается неопределенность констрейнтов. Auto Layout на основе этой неопределенности имеет множество решений (height для stackView и statusButton), выбирая оптимальное, а не необходимое, то есть вместо необходимых 50pts для statusButton растягивается stackView.
+        
+        NSLayoutConstraint.activate([
+            topConstraint, leadingConstraint, trailingConstraint,
+            imageViewAspectRatio,
+            self.buttonTopConstraint, leadingButtonConstraint,
+            trailingButtonConstraint, bottomButtonConstraint, heightButtonConstraint
+        ].compactMap({ $0 }))
+    }
+    
+    @objc private func didTapStatusButton() {
+        if self.textField.isHidden {
+            self.addSubview(self.textField)
+            
+//            self.buttonTopConstraint?.isActive = false // Если констрейнт будет жестким, необходимо деактивировать его, иначе будет конфликт констрейнтов, и Auto Layout не сможет однозначно определить фреймы textField'а.
+            
+            let topConstraint = self.textField.topAnchor.constraint(equalTo: self.infoStackView.bottomAnchor, constant: 10)
+            let leadingConstraint = self.textField.leadingAnchor.constraint(equalTo: self.statusLabel.leadingAnchor)
+            let trailingConstraint = self.textField.trailingAnchor.constraint(equalTo: self.infoStackView.trailingAnchor)
+            let heightTextFieldConstraint = self.textField.heightAnchor.constraint(equalToConstant: 34) // Не указав высоту textField'а, получается неоднозначность/неопределенность констрейнтов. Auto Layout на основе этой неопределенности имеет множество решений (height для stackView, textField), выбирая оптимальное, а не необходимое, то есть вместо 34pts для textField'а растягивается stackView.
+            self.buttonTopConstraint = self.statusButton.topAnchor.constraint(equalTo: self.textField.bottomAnchor, constant: 20)
+            
+            NSLayoutConstraint.activate([
+                topConstraint, leadingConstraint, trailingConstraint, heightTextFieldConstraint, self.buttonTopConstraint
+            ].compactMap({ $0 }))
+        } else {
+            #warning("Убрать textField из вью!")
+        }
+        
+        self.delegate?.didTapStatusButton(textFieldIsVisible: self.textField.isHidden) { [weak self] in
+            self?.textField.isHidden.toggle()
+        }
+    }
+}
